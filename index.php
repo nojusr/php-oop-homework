0de96d31a0da432452d1ce550b58169bd0af8bb2
Nojus R <nojusr@gmail.com>
2019-11-11 00:37:02 +0200

    updated index.php

--- index.php
+++ index.php
@@ -2,22 +2,26 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$test = new objects\Apartament(1345, 99);
+$test = new Entities\Apartament(1345, 99);
 
-$guest1 = new objects\Guest("Antanas", "Zanavykas");
+$guest1 = new Entities\Guest("Antanas", "Zanavykas");
 $startDate = new DateTime("2019-04-20");
 $endDate = new DateTime("2019-04-25");
-$res = new objects\Reservation($startDate, $endDate, $guest1);
+$res = new Entities\Reservation($startDate, $endDate, $guest1);
 
-$guest3 = new objects\Guest("Bronius", "Šimonius");
+$guest3 = new Entities\Guest("Bronius", "Šimonius");
 $sd3 = new DateTime("2019-05-9");
 $ed3 = new DateTime("2019-05-15");
-$res3 = new objects\Reservation($sd3, $ed3, $guest3);
+$res3 = new Entities\Reservation($sd3, $ed3, $guest3);
 
-$guest2 = new objects\Guest("Petras", "Kapšelis");
-$sd2 = new DateTime("2019-05-14");
-$ed2 = new DateTime("2019-05-30");
-$res2 = new objects\Reservation($sd2, $ed2, $guest2);
+$guest2 = new Entities\Guest("Petras", "Kapšelis");
+$sd2 = new DateTime("2019-05-8");
+$ed2 = new DateTime("2019-05-14");
+$res2 = new Entities\Reservation($sd2, $ed2, $guest2);
 
-objects\BookingManager::bookRoom($test, $res);
+
+
+
+Statics\BookingManager::bookRoom($test, $res2);
+Statics\BookingManager::bookRoom($test, $res3);
 
